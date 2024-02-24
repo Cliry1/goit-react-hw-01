67bc4b461285ff3f23f2a96e05a771511c9e3c1d
PRO100 Cliry <128536194+Cliry1@users.noreply.github.com>
2024-02-24 21:55:40 +0200

    new

--- src/components/friend/FriendList.jsx
+++ src/components/friend/FriendList.jsx
@@ -1,5 +1,5 @@
 import css from "./FriendList.module.css"
-import clsx from "clsx";
+import FriendListItem from"./FriendListItem"
 export default function FriendList({friends}) {
 
 
@@ -7,18 +7,12 @@ export default function FriendList({friends}) {
     <ul className={css.friendList}>
       {
         friends.map(friend =>{
-          return (
-          <li className={css.friendListItem} key={friend.id}>
-            <div>
-              <img src={friend.avatar} alt="Avatar" width="48" />
-              <p className={css.friendListItemName}>{friend.name}</p>
-              {
-                friend.isOnline ? <p className={clsx(css.friendListItemOnline, css.friendListItemStatus)}>Online</p > : <p className={clsx(css.friendListItemOffline, css.friendListItemStatus)}>Offline</p>
-              }
-              
-            </div>
-          </li>
-          )})
+          return(
+            <li className={css.friendListItem} key={friend.id}>
+              <FriendListItem friend={friend} />
+            </li>
+          )
+        })
       }
     </ul>
   )
